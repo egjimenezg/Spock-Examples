2e21241bbb3909dae437687d975993edcf578547
egjimenezg <egjimenezg@gmail.com>
2016-02-26 22:24:17 -0600

    coding #3 updating service and removing test

--- core/src/main/groovy/com/makingdevs/service/impl/AddressServiceImpl.groovy
+++ core/src/main/groovy/com/makingdevs/service/impl/AddressServiceImpl.groovy
@@ -2,11 +2,13 @@ package com.makingdevs.service.impl
 
 import com.makingdevs.service.AddressService
 import com.makingdevs.service.SepomexService
+import com.makingdevs.service.OperationDBAddressService
 import com.makingdevs.Address
 
 class AddressServiceImpl implements AddressService{
 
   SepomexService sepomexService = new SepomexServiceImpl()
+  OperationDBAddressService operationDBAddressServiceImpl = new OperationDBAddressServiceImpl()
 
   def getAddressByPostalCode(postalCode){
     def addressInfo = sepomexService.getDataForPostalCode(postalCode)
@@ -15,6 +17,10 @@ class AddressServiceImpl implements AddressService{
                 town:addressInfo.town,
                 state:addressInfo.state,
                 city:addressInfo.city)
+  }
+
+  def saveAddress(Address address){
+    operationDBAddressServiceImpl.insertAddress(address)
   }
 
 }
